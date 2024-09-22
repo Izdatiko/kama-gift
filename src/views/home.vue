8ec118d012c0635f667dc4afe54a19598438217f
ogayvladislav47@gmail.com <chebupeli>
2024-09-22 21:28:07 +0500

    test

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,7 +1,19 @@
 <template>
     <div>
-        <Swiper />
+        <Swiper v-if="isSwiper"/>
+        <video width="100%" height="360" controls v-if="isFlex">
+            <source src="/assets/flex.MOV?url" type="video/mp4">
+          </video>
+          <video width="100%" height="360" controls v-if="moreFlex">
+            <source src="/assets/flex2.MOV?url" type="video/mp4">
+          </video>
+
         <p>{{ text }}</p>
+
+        <button v-if="isSwiper" @click="show">
+            Показать flex
+        </button>
+        <button @click="showMoreFlex" v-if="!isSwiper">Еще больше flex</button>
     </div>
 </template>
 
@@ -11,6 +23,22 @@ import axios from "axios";
 import { ref, onMounted, onBeforeUnmount } from "vue";
 
 const text = ref('');
+
+const moreFlex = ref(false)
+
+const isFlex = ref(false)
+
+const isSwiper = ref(true);
+
+const show = () => {
+    isSwiper.value = false
+    isFlex.value = true
+}
+
+const showMoreFlex = () => {
+    isFlex.value = false
+    moreFlex.value = true
+}
 
 const getText = async () => {
     try {
